562d294f3f67dc5ff2d188a4ca46411773386e5c
prithviBytes <prithvisuvarna56@gmail.com>
2021-11-15 15:57:15 +0530

    add: templates

--- course/urls.py
+++ course/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view
+from pages.views import contact_view
 
 urlpatterns = [
     path('', home_view),
+    path('contact', contact_view),
     path('admin/', admin.site.urls),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Home Page</h1>")
+def home_view(request, *args, **kwargs):
+    return render(request, 'home.html', {})
+
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h1>Contact Page!!!</h1>")
